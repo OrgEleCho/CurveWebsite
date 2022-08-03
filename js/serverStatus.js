477a23a67b1d63fcbfd2e028dd5c6e1b84051c98
李家骏 <geometry2019@Outlook.com>
2022-08-03 18:38:47 +0800

    写了获取服务器信息

---
+++ js/serverStatus.js
@@ -0,0 +1,28 @@
+function httpRequest(obj) {
+    //兼容性处理
+    var xhr = null
+    try {
+        xhr = new XMLHttpRequest()
+    } catch (error) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xhr.open('get', obj, false)
+    xhr.send(null)
+
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        return xhr.response
+    } else {
+        return undefined
+    }
+
+}
+function getServerStatus() {
+    var url = "https://api.bedrockinfo.com/v2/status?server=121.43.188.225&port=11264"
+    var sUrl = url + '#' + (new Date()).getTime()
+    var a = httpRequest(sUrl)
+    if (a === undefined) {
+        return undefined
+    } else {
+        return JSON.parse(a)
+    }
+}
